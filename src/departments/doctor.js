0f0ce197b121909111aed6b98d94c6798d545366
johann219 <147496124+johann219@users.noreply.github.com>
2026-03-26 21:01:22 +0300

    Fetch and rehydrate json storage

--- src/departments/doctor.js
+++ src/departments/doctor.js
@@ -1,11 +1,14 @@
 export default class Doctor {
-    constructor ( {name, surname, patronymic, specialty, accolades, department} ) {
+    constructor ( {name, surname, patronymic, specialty, accolades, department, photoAddress, hasAppointments, isDepartmentHead} ) {
         this.name = name;
         this.surname = surname;
         this.patronymic = patronymic;
         this.specialty = specialty;
         this.accolades = accolades;
         this.department = department;
+        this.photoAddress = photoAddress;
+        this.hasAppointments = hasAppointments;
+        this.isDepartmentHead = isDepartmentHead;
         this.employeeID = crypto.randomUUID();
     }
 }
--- src/index.js
+++ src/index.js
@@ -2,117 +2,56 @@ import './styles/styles.css';
 import Doctor from './departments/doctor.js';
 import { SELECTOR } from './const.js';
 
-const doctorRegistry = [];
-
 const doctorListElement = document.querySelector(SELECTOR.DOCTOR_LIST);
 const doctorCardTemplateElement = document.querySelector(SELECTOR.DOCTOR_CARD_TEMPLATE);
 
-const printDoctorInfo = (doctor) => {
-    console.log(`${doctor.surname} ${doctor.name} ${doctor.patronymic} is a ${doctor.specialty}, ${doctor.accolades}, who works at ${doctor.department}. DEBUG INFO: Employee ID = ${doctor.employeeID}`);
-};
+const getDoctorsData = async () => {
+    const response = await fetch('/doctorsData.json');
 
-const addDoctor = (doctor) => {
-    const newDoctor = new Doctor(doctor);
-    doctorRegistry.push(newDoctor);
-}; 
+    console.log(response);
 
-addDoctor({
-    name: 'Ivan', 
-    surname: 'Ivanov', 
-    patronymic: 'Ivanovich', 
-    specialty: 'Neurosurgeon', 
-    accolades: 'Doctor of Medical Science', 
-    department: 'Neurosurgery Department'
-});
+    const doctorsData = await response.json();
 
-addDoctor({
-    name: 'Petr',
-    patronymic: 'Petrovich', 
-    surname: 'Petrov', 
-    department: 'Surgery Department',
-    specialty: 'General Surgeon', 
-    accolades: 'Candidate of Medical Science', 
-});
+    console.log(doctorsData);
+};  
 
-addDoctor({
-    name: 'Ivan', 
-    surname: 'Ivanov', 
-    patronymic: 'Ivanovich', 
-    specialty: 'Neurosurgeon', 
-    accolades: 'Doctor of Medical Science', 
-    department: 'Neurosurgery Department'
-});
+const plainDoctorRegistry = getDoctorsData();
 
-addDoctor({
-    name: 'Petr',
-    patronymic: 'Petrovich', 
-    surname: 'Petrov', 
-    department: 'Surgery Department',
-    specialty: 'General Surgeon', 
-    accolades: 'Candidate of Medical Science', 
-});addDoctor({
-    name: 'Ivan', 
-    surname: 'Ivanov', 
-    patronymic: 'Ivanovich', 
-    specialty: 'Neurosurgeon', 
-    accolades: 'Doctor of Medical Science', 
-    department: 'Neurosurgery Department'
-});
+const rehydrateDoctorRegistry = plainDoctorRegistry.map((plainDoctorObject) => new Doctor(plainDoctorObject));
 
-addDoctor({
-    name: 'Petr',
-    patronymic: 'Petrovich', 
-    surname: 'Petrov', 
-    department: 'Surgery Department',
-    specialty: 'General Surgeon', 
-    accolades: 'Candidate of Medical Science', 
-});addDoctor({
-    name: 'Ivan', 
-    surname: 'Ivanov', 
-    patronymic: 'Ivanovich', 
-    specialty: 'Neurosurgeon', 
-    accolades: 'Doctor of Medical Science', 
-    department: 'Neurosurgery Department'
-});
+const doctorRegistry = rehydrateDoctorRegistry(plainDoctorRegistry);
 
-addDoctor({
-    name: 'Petr',
-    patronymic: 'Petrovich', 
-    surname: 'Petrov', 
-    department: 'Surgery Department',
-    specialty: 'General Surgeon', 
-    accolades: 'Candidate of Medical Science', 
-});
+// const createDoctorCardElement = (doctor) => {
+//     const newDoctorCardFragment = doctorCardTemplateElement.content.cloneNode(true);
 
-const createDoctorCardElement = (doctor) => {
-    const newDoctorCardFragment = doctorCardTemplateElement.content.cloneNode(true);
+//     const newDoctorPhotoElement = 
 
-    const newDoctorCardFioElement = newDoctorCardFragment.querySelector(SELECTOR.DOCTOR_CARD_FIO);
-    newDoctorCardFioElement.textContent = `${doctor.surname} ${doctor.name} ${doctor.patronymic}`;
+//     const newDoctorCardFioElement = newDoctorCardFragment.querySelector(SELECTOR.DOCTOR_CARD_FIO);
+//     newDoctorCardFioElement.textContent = `${doctor.surname} ${doctor.name} ${doctor.patronymic}`;
 
-    const newDoctorCardSpecialtyElement = newDoctorCardFragment.querySelector(SELECTOR.DOCTOR_CARD_SPECIALTY);
-    newDoctorCardSpecialtyElement.textContent = doctor.specialty;
+//     const newDoctorCardSpecialtyElement = newDoctorCardFragment.querySelector(SELECTOR.DOCTOR_CARD_SPECIALTY);
+//     newDoctorCardSpecialtyElement.textContent = doctor.specialty;
 
-    const newDoctorCardAccoladesElement = newDoctorCardFragment.querySelector(SELECTOR.DOCTOR_CARD_ACCOLADES);
-    newDoctorCardAccoladesElement.textContent = doctor.accolades;
+//     const newDoctorCardAccoladesElement = newDoctorCardFragment.querySelector(SELECTOR.DOCTOR_CARD_ACCOLADES);
+//     newDoctorCardAccoladesElement.textContent = doctor.accolades;
 
-    return newDoctorCardFragment;
-}
+//     return newDoctorCardFragment;
+// }
 
-const renderDoctorList = (doctorList) => {
-    doctorListElement.replaceChildren();
+// const renderDoctorList = (doctorList) => {
+//     doctorListElement.replaceChildren();
 
-    const newFragment = new DocumentFragment();
+//     const newFragment = new DocumentFragment();
 
-    doctorList.forEach((doctor) => {
-        const newDoctorCardElement = createDoctorCardElement(doctor);
-        newFragment.append(newDoctorCardElement);
-    })
+//     doctorList.forEach((doctor) => {
+//         const newDoctorCardElement = createDoctorCardElement(doctor);
+//         newFragment.append(newDoctorCardElement);
+//     })
 
-    doctorListElement.append(newFragment);
-};
+//     doctorListElement.append(newFragment);
+// };
 
-console.log(doctorListElement);
-console.log(doctorCardTemplateElement);
+// console.log(doctorListElement);
+// console.log(doctorCardTemplateElement);
 
-renderDoctorList(doctorRegistry);
+// renderDoctorList(doctorRegistry);
