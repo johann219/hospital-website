99b5665947b1435baf7d3aefb0904e8adaefd22f
johann219 <147496124+johann219@users.noreply.github.com>
2026-04-04 20:40:45 +0300

    Add html parsing

--- utils.js
+++ utils.js
@@ -2,6 +2,20 @@ const capitalizeFirstLetter = (string) => {
     return string.slice(0, 1).toUpperCase() + string.slice(1);
 };
 
+const parseHTML = (htmlString) => {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(htmlString, 'text/html');
+
+    const fragment = document.createDocumentFragment();
+
+    while (doc.body.firstChild) {
+        fragment.append(doc.body.firstChild);
+    }
+
+    return fragment;
+};
+
 export const Utils = {
     capitalizeFirstLetter,
+    parseHTML,
 }
